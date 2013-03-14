7bb490b4a9bc2155924ebffbc381e5c7aa73c8d1
mcneves <mcn@isep.ipp.pt>
2013-03-14 11:06:13 +0000

    criar projeto com linha1 e linha2

---
+++ PL5/src/pl5/PL5.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl5;
+
+/**
+ *
+ * @author mcn
+ */
+public class PL5 {
+
+      /**
+       * @param args the command line arguments
+       */
+      public static void main(String[] args) {
+            System.out.println("Linha1");
+            System.out.println("Linha2");
+      }
+}
